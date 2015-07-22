ab1bc2314f2625d9632dede6e7a3d03461302c79
Roni Choudhury <roni.choudhury@kitware.com>
2015-07-22 15:51:22 -0400

    Got basic clique view into a pane

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,31 @@
 /*jshint browser: true, jquery: true */
-/*global*/
+/*global _, clique, tangelo */
 
 $(function () {
     "use strict";
 
-    console.log("hello, world");
+    var initialize;
+
+    initialize = function (cfg) {
+        var graph,
+            view;
+
+        // Initialize Clique.
+        window.graph = graph = new clique.Graph({
+            adapter: tangelo.getPlugin("mongo").Mongo,
+            options: {
+                host: cfg.graphHost,
+                database: cfg.graphDatabase,
+                collection: cfg.graphCollection
+            }
+        });
+
+        view = new clique.view.Cola({
+            model: graph,
+            el: "#clique"
+        });
+    };
+
+    $.getJSON("assets/config.json")
+        .then(initialize, _.bind(initialize, {}));
 });
