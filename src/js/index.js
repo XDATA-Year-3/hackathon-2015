a7c8779599414405560d3c2de90f9de72fcd8ac1
Roni Choudhury <roni.choudhury@kitware.com>
2015-07-23 16:41:26 -0400

    Adding dummy delv interaction

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,29 @@
 /*jshint browser: true, jquery: true */
-/*global _, clique, tangelo */
+/*global _, clique, tangelo, app, delv */
 
 $(function () {
     "use strict";
 
-    var initialize;
+    var initialize,
+        DummyView;
+
+    window.app = {};
+
+    DummyView = function () {
+        return {
+            react: function (invoker, dataset, attribute) {
+                console.log("invoker", invoker);
+                console.log("dataset", dataset);
+                console.log("attribute", attribute);
+            }
+        };
+    };
 
     initialize = function (cfg) {
         var graph,
-            view;
+            view,
+            cliqueSelection,
+            dummy;
 
         // Initialize Clique.
         window.graph = graph = new clique.Graph({
@@ -24,6 +39,35 @@ $(function () {
             model: graph,
             el: "#clique"
         });
+
+        // Initialize Delv.
+        app.delv = {};
+        app.delv.dataIF = new delv.data("data");
+        delv.addDataIF(app.delv.dataIF);
+        app.delv.dataIF.setDelvIF(delv);
+        cliqueSelection = app.delv.dataIF.addDataSet("clique_selection");
+
+        app.delv.clique = new delv.d3View("clique");
+        app.delv.clique.dataIF("data");
+
+        view.model.on("change", function (m) {
+            var keys = _.pluck(m.get("nodes"), "key");
+            cliqueSelection.clearItems();
+
+            _.each(keys, function (key) {
+                cliqueSelection.addId(key);
+            });
+        });
+
+        view.selection.on("change", _.debounce(function (sel) {
+            var delvView = app.delv.clique;
+            delvView._dataIF.updateSelectedIds(delvView.svgElem, "clique_selection", _.keys(sel.attributes));
+        }, 100));
+
+        dummy = new DummyView();
+        delv.addView(dummy, "dummy");
+
+        delv.connectToSignal("selectedIdsChanged", "dummy", "react");
     };
 
     $.getJSON("assets/config.json")
